edfbe887e3df32e6dd067a3775a404e3b57cbefd
AndriAlbatroz <michelin.98.bs@gmail.com>
2017-02-11 11:39:52 +0100

    Lol

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var Discord = require('discord.js');
 var ytdl = require('ytdl-core');
+var fs = require('fs');
+var youtube = require('youtube-node');
+var jsonfile = require('jsonfile');
 
 const client = new Discord.Client();
 const streamOptions = {seek: 0, volume: 1};
+const yt = new youtube();
 
 var info = require('./auth.json');
+var initYT = 'http://www.youtube.com/watch?v=';
+
+yt.setKey(info.ytAPI);
 
 client.login(info.token);
 
@@ -22,15 +29,42 @@ client.on('message', msg => {
 			"[-] !calc y=x+1 - Per usare le funzionalità di wolframAlpha\n" +
 			"[-] !sfolla     - Per farmi sfollare a caso\n" + 
 			"[-] !connect    - Per connetersi ad un canale vocale\n" + 
+			"[-] !leave      - Per disconnetersi da una stanza vocale\n" +
 			"Per adesso solo questo.. ma ci stiamo lavorando."
 		);
 	}
 
-	if(msg.content == '!connect') {
-		for(var obj of client.channels) {
-			for(var ob of obj) {
-				console.log(ob.guild.channels);
-			}
-		}
+	if(msg.content === '!connect') {
+		var channel = msg.member.voiceChannel;
+		channel.join().then( () => {
+			console.log('Entrato nel canale' + " " + channel)
+		}).catch(console.error);
 	}
+
+	if(msg.content == '!leave') {
+		var channel = msg.member.voiceChannel;
+		channel.leave();
+	}
+
+	if(msg.content.startsWith('!play ')) {
+		var title = msg.content.replace('!play ','');
+		var videoID;
+		yt.search(title, 2, (err,res) => {
+			if(err) throw err;
+			jsonfile.writeFile('prova.json',res,'utf8', err => {
+				if(err) throw err;
+			});
+			for(var key in res.items) {
+				if(res.items[key].id.kind == 'youtube#video') {
+					if(videoID == null) {	
+						videoID = res.items[key].id.videoId;
+						break;
+					}
+				}
+			}
+			var URL = initYT + videoID;
+			ytdl(URL).pipe(fs.createWriteStream('prova.mp3'));
+		});
+	}
+
 });
